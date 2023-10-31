1ef0ff7d59b91dcbab9c1bbbbd377bf581d203b3
sein yang <did756984@naver.com>
2023-10-31 14:28:02 +0900

    [feature/concave] 오목 구현(4*3 대각선 판정 )

--- src/main/java/com/example/gamebackend/service/OmokService.java
+++ src/main/java/com/example/gamebackend/service/OmokService.java
@@ -16,17 +16,16 @@ public class OmokService {
 
         if (isEmptySpot(color, location, numbers)) {
             return " 돌을 놓을 수 없습니다";
-        } else if (is3X3(color,location,numbers)) {
+        } else if (is3X3(color, location, numbers)) {
             return "3*3판정입니다";
-        } else if (is4X3(color,location,numbers)) {
+        } else if (is4X3(color, location, numbers)) {
             return "4*3판정입니다";
-        } else if(isBlackWin(color,location,numbers)) {
+        } else if (isBlackWin(color, location, numbers)) {
             return "흑돌승리 판정";
         }
 
         // 백돌승리판정
         // else 그냥놔도됨
-
 
 
         return "그냥 놔도됨";
@@ -42,7 +41,7 @@ public class OmokService {
             numbers[i] = rows[i].split("");
         }
 
-     return numbers;
+        return numbers;
 
     }
 
@@ -91,7 +90,7 @@ public class OmokService {
     }
 
     //4*3 판정
-    public boolean is4X3(String color,String location,String[][] numbers) {
+    public boolean is4X3(String color, String location, String[][] numbers) {
 
         int x = Integer.parseInt(location.split(",")[0]);       //좌표값의 x축
         int y = Integer.parseInt(location.split(",")[1]);       //좌표값의 y축
@@ -101,42 +100,41 @@ public class OmokService {
         int count = 0;
 
         //가로방향으로 4개의 돌이 연속되는지 확인,가로방향으로 -2,+2옮겨가며 확인
-        for (int i = x - 2; i <= x + 2; i++) {
-                //바둑판을 벗어나면 건너띈다
-                if (i < 0 || i > 12) continue;
-                //연속된 같은 돌색인지 판정
-                boolean isSameColor = true;
-                //가로로 3칸을 검사하고
-                for (int j = i; j < i + 3; j++) {
-                if (j < 0 || j > 12) continue;
+        for (int i = x - 3; i <= x ; i++) {
+            //바둑판을 벗어나면 건너띈다
+            if (i < 0 || i+3 > 12) continue;
+            //연속된 같은 돌색인지 판정
+            boolean isSameColor = true;
+            //가로로 3칸을 검사하고
+            for (int j = i; j < i + 4; j++) {
                 //만약 연속되는 색이 다르다면 false
                 if (!numbers[j][y].equals(targetColor)) {
                     isSameColor = false;
                     break;
                 }
             }
-                //가로로 연속된값이면 count증가
-                if (isSameColor) {
-                    count++;
-                    System.out.println("Horizontal: " + count);
-                    break;
-                }
+            //가로로 연속된값이면 count증가
+            if (isSameColor) {
+                count++;
+                System.out.println("Horizontal: " + count);
+                break;
             }
+        }
 
         // 세로 방향으로 4개가 연속되는지 확인
-        for (int j = y - 2; j <= y+2; j++) {
+        for (int j = y - 3; j <= y ; j++) {
 
-                if (j < 0 || j+2 > 12) continue;
-                boolean isSameColor = true;
+            if (j < 0 || j+3> 12) continue;
+            boolean isSameColor = true;
 
-                for (int i = j ; i < j + 3; i++) {
+            for (int i = j; i < j + 4; i++) {
 
                 if (!numbers[x][i].equals(targetColor)) {
                     isSameColor = false;
                     break;
                 }
             }
-                if (isSameColor) {
+            if (isSameColor) {
 
                 count++;
                 System.out.println("Vertical: " + count);
@@ -145,93 +143,223 @@ public class OmokService {
             }
         }
 
+        //대각선 우상향으로 4*3 4확인
+        for (int i = x - 3, j = y + 3; i <= x && j >= y; i++, j--) {
+            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+
+            boolean isSameColor = true;
+
+            for (int k = 0; k < 4; k++) {
+                if (!numbers[i][j].equals(targetColor)) {
+                    isSameColor = false;
+                    break;
+                }
+            }
+            if (isSameColor) {
+                return true;
+            }
+        }
+
+        //대각선 우상향으로 4*3 3확인
+        for(int i = x - 2, j =y + 2; i<=x && j>=y; i++,j--){
+            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+
+            boolean isSameColor = true;
+
+            for (int k = 0; k < 3; k++) {
+                if (!numbers[i][j].equals(targetColor)) {
+                    isSameColor = false;
+                    break;
+                }
+            }
+            if (isSameColor) {
+                return true;
+            }
+        }
+
+        //대각선 우하향으로 4*3 4확인
+        for(int i= x + 3, j = y + 3; i<=x && j<=y ;i--,j++) {
+            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+
+            boolean isSameColor = true;
+
+            for (int k = 0; k < 4; k++) {
+                if (!numbers[i][j].equals(targetColor)) {
+                    isSameColor = false;
+                    break;
+                }
+            }
+            if (isSameColor) {
+                return true;
+            }
+        }
+        //대각선 우하향으로 4*3 3확인
+        for(int i = x + 2, j = y + 2 ; i>=x && j>=x ; i--,j--) {
+            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+
+            boolean isSameColor = true;
+
+            for (int k = 0; k < 3; k++) {
+                if (!numbers[i][j].equals(targetColor)) {
+                    isSameColor = false;
+                    break;
+                }
+            }
+            if (isSameColor) {
+                return true;
+            }
+        }
+
+        //대각선 좌상향으로 4*3 4확인
+        for(int i = x - 3 , j= y -3 ; i<=x && j<=y ; i++,j++) {
+            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+
+            boolean isSameColor = true;
+
+            for (int k = 0; k < 4; k++) {
+                if (!numbers[i][j].equals(targetColor)) {
+                    isSameColor = false;
+                    break;
+                }
+            }
+            if (isSameColor) {
+                return true;
+            }
+        }
+        //대각선 좌상향으로 4*3 3확인
+        for(int i= x-2, j = y-2 ;i<=x && j<=y ; i++ ,j++ ) {
+            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+
+            boolean isSameColor = true;
+
+            for (int k = 0; k < 3; k++) {
+                if (!numbers[i][j].equals(targetColor)) {
+                    isSameColor = false;
+                    break;
+                }
+            }
+            if (isSameColor) {
+                return true;
+            }
+        }
+
+        //대각선 좌하향으로 4*3 4확인
+        for(int i= x + 3 ,j= y - 3 ; i>=x && j<=y ;i--,j++) {
+            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+
+            boolean isSameColor = true;
+
+            for (int k = 0; k < 4; k++) {
+                if (!numbers[i][j].equals(targetColor)) {
+                    isSameColor = false;
+                    break;
+                }
+            }
+            if (isSameColor) {
+                return true;
+            }
+        }
+
+        //대각선 좌하향으로 4*3 3확인
+        for(int i = x+2, j = y-2 ; i>=x && j<=y ;i--,j++) {
+            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+
+            boolean isSameColor = true;
+
+            for (int k = 0; k < 3; k++) {
+                if (!numbers[i][j].equals(targetColor)) {
+                    isSameColor = false;
+                    break;
+                }
+            }
+            if (isSameColor) {
+                return true;
+            }
+        }
         return count > 0;
     }
 
-    public boolean isBlackWin(String color,String location,String[][]numbers){
-        int x = Integer.parseInt(location.split(",")[0]);
-        int y = Integer.parseInt(location.split(",")[1]);
-        String targetColor = (color.equals("1")) ? "1" : "2";
+        public boolean isBlackWin (String color, String location, String[][]numbers){
+            int x = Integer.parseInt(location.split(",")[0]);
+            int y = Integer.parseInt(location.split(",")[1]);
+            String targetColor = (color.equals("1")) ? "1" : "2";
 
-        int count = 0;
+            int count = 0;
 
-        //가로 방향으로 5개가 연속되는지 확인
-        for(int i=x-4; i<=x; i++){
-            //오목판 안에서 연속되는값 i가 13*13 오목판에서 놀면 통과
-            if (i<0 || i+4>12) continue;
-            //연속되는 오목 색깔이 동일한지
-            boolean isSameColor = true;
-            //지금 위에서 검사한 i의 값에 5개 돌을 검사위한 반복문
-            for (int j=i; j<+5 ;j++){
-                //현재 검사하는 돌색과 현재 돌색이 다르면 false
-                if(!numbers[j][y].equals(targetColor)){
-                    isSameColor=false;
-                    break;
+            //가로 방향으로 5개가 연속되는지 확인
+            for (int i = x - 4; i <= x; i++) {
+                //오목판 안에서 연속되는값 i가 13*13 오목판에서 놀면 통과
+                if (i < 0 || i + 4 > 12) continue;
+                //연속되는 오목 색깔이 동일한지
+                boolean isSameColor = true;
+                //지금 위에서 검사한 i의 값에 5개 돌을 검사위한 반복문
+                for (int j = i; j < +5; j++) {
+                    //현재 검사하는 돌색과 현재 돌색이 다르면 false
+                    if (!numbers[j][y].equals(targetColor)) {
+                        isSameColor = false;
+                        break;
+                    }
+                }
+                //같은 색이라면 true
+                if (isSameColor) {
+                    return true;
                 }
             }
-            //같은 색이라면 true
-            if (isSameColor){
-                return true;
-            }
-        }
 
-       //세로 방향으로 5개가 연속되는지 확인
-        for(int j=y-4;j<=y; j++){
-            //j가 원하는 범위내에서 있는지 확인하고 넘어감
-            if(j<0 || j+4>12)continue;
-            //세로로 5개 돌이 모두 동일한 색인지
-            boolean isSameColor = true;
+            //세로 방향으로 5개가 연속되는지 확인
+            for (int j = y - 4; j <= y; j++) {
+                //j가 원하는 범위내에서 있는지 확인하고 넘어감
+                if (j < 0 || j + 4 > 12) continue;
+                //세로로 5개 돌이 모두 동일한 색인지
+                boolean isSameColor = true;
 
-            for(int i=j; i<j+5 ;i++){
-                //만약 현재 돌 색(x)이 검사하려는 현재 검사하려는 돌의 색과 다르다면 false
-                if (!numbers[x][i].equals(targetColor)){
-                    isSameColor=false;
-                    break;
+                for (int i = j; i < j + 5; i++) {
+                    //만약 현재 돌 색(x)이 검사하려는 현재 검사하려는 돌의 색과 다르다면 false
+                    if (!numbers[x][i].equals(targetColor)) {
+                        isSameColor = false;
+                        break;
+                    }
+                }
+                if (isSameColor) {
+                    return true;
                 }
             }
-            if (isSameColor){
-                return true;
-            }
-        }
 
-        // 대각선 방향 (우상향)으로 5개가 연속되는지 확인
-        //대각선에 4칸 위치한 방향으로 현재 놓인돌에 5칸까지 검사
-        for (int i = x - 4, j = y + 4; i <= x && j >= y; i++, j--) {
-            //만약,i가 오목(13*13)판에 있거나 음수이면 검사할필요없으니 통과
-            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+            // 대각선 방향 (우상향)으로 5개가 연속되는지 확인
+            //대각선에 4칸 위치한 방향으로 현재 놓인돌에 5칸까지 검사
+            for (int i = x - 4, j = y + 4; i <= x && j >= y; i++, j--) {
+                //만약,i가 오목(13*13)판에 있거나 음수이면 검사할필요없으니 통과
+                if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
 
-            boolean isSameColor = true;
+                boolean isSameColor = true;
 
-            for (int k = 0; k < 5; k++) {
-                if (!numbers[i + k][j - k].equals(targetColor)) {
-                    isSameColor = false;
-                    break;
+                for (int k = 0; k < 5; k++) {
+                    if (!numbers[i + k][j - k].equals(targetColor)) {
+                        isSameColor = false;
+                        break;
+                    }
+                }
+                if (isSameColor) {
+                    return true;
                 }
             }
-            if (isSameColor) {
-                return true;
-            }
-        }
 
-        // 대각선 방향 (우하향)으로 5개가 연속되는지 확인
-        for (int i = x - 4, j = y - 4; i <= x && j <= y; i++, j++) {
-            if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
-            boolean isSameColor = true;
-            for (int k = 0; k < 5; k++) {
-                if (!numbers[i + k][j + k].equals(targetColor)) {
-                    isSameColor = false;
-                    break;
+            // 대각선 방향 (우하향)으로 5개가 연속되는지 확인
+            for (int i = x - 4, j = y - 4; i <= x && j <= y; i++, j++) {
+                if (i < 0 || j < 0 || i + 4 > 12 || j + 4 > 12) continue;
+                boolean isSameColor = true;
+                for (int k = 0; k < 5; k++) {
+                    if (!numbers[i + k][j + k].equals(targetColor)) {
+                        isSameColor = false;
+                        break;
+                    }
+                }
+                if (isSameColor) {
+                    return true;
                 }
             }
-            if (isSameColor) {
-                return true;
-            }
-        }
 
-        return false;
+            return false;
+        }
     }
-
-
-
-}
 

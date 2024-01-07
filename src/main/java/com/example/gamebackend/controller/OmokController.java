1fff9ab194ecdf6ea20b3d66c541fef9a45029da
jinwooooooo <ko440338@gmail.com>
2024-01-07 16:18:53 +0900

    CORS 에러 조치 2

--- src/main/java/com/example/gamebackend/controller/OmokController.java
+++ src/main/java/com/example/gamebackend/controller/OmokController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(originPatterns = "*", allowedHeaders = "*")
 public class OmokController {
 
     private final OmokService omokService;
